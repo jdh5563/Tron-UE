836c324b469d7cb97ea98eec480de028db755fa1
John Heiden (RIT Student) <jdh5563@rit.edu>
2024-04-03 11:32:04 -0400

    Added third-person starter + c++

---
+++ Source/TronUE/GenerateLevel.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GenerateLevel.h"
+
+// Sets default values
+AGenerateLevel::AGenerateLevel()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGenerateLevel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGenerateLevel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
